0a102979820cb75ef297ffed3d5f57810780b6ea
jjvbarbosa <joaovitor123war@hotmail.com>
2022-09-14 16:07:35 -0300

    Method to insert a new product using POST

--- src/main/java/com/jvbarbosa/dscommerce/DscommerceApplication.java
+++ src/main/java/com/jvbarbosa/dscommerce/DscommerceApplication.java
@@ -9,5 +9,4 @@ public class DscommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DscommerceApplication.class, args);
 	}
-
 }
--- src/main/java/com/jvbarbosa/dscommerce/controllers/ProductController.java
+++ src/main/java/com/jvbarbosa/dscommerce/controllers/ProductController.java
@@ -5,10 +5,7 @@ import com.jvbarbosa.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -25,5 +22,10 @@ public class ProductController {
     @GetMapping
     public Page<ProductDTO> findAll(Pageable pageable) {
         return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ProductDTO insert(@RequestBody ProductDTO dto) {
+        return service.insert(dto);
     }
 }
