e7b6a47527b9df04f1a2266673287bd1e6b0f315
jjvbarbosa <joaovitor123war@hotmail.com>
2022-09-15 14:33:05 -0300

    Bean Validation

--- src/main/java/com/jvbarbosa/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/jvbarbosa/dscommerce/dto/ProductDTO.java
@@ -1,12 +1,20 @@
 package com.jvbarbosa.dscommerce.dto;
 
 import com.jvbarbosa.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 3, max = 80, message = "Nome precisa ter entre 3 e 80 caracteres")
     private String name;
+    @NotBlank(message = "Campo requerido")
+    @Size(min = 10, message = "Descrição precis ter no mínimo 10 caracteres")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
