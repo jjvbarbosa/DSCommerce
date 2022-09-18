d3f22149d4df6a372df7de4d613dcb893c7249c3
João Vitor Barbosa <joaovitor@jv.localdomain>
2022-09-18 10:42:09 -0300

    Search products by name

--- src/main/java/com/jvbarbosa/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/jvbarbosa/dscommerce/repositories/ProductRepository.java
@@ -1,7 +1,15 @@
 package com.jvbarbosa.dscommerce.repositories;
 
 import com.jvbarbosa.dscommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT obj FROM Product obj "
+            + "WHERE UPPER(obj.name) "
+            + "LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Product> searchByName(String name, Pageable pageable);
 }
