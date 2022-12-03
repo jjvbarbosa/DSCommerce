42509892bc2586785bdf0b7f8f4dc43f028daa05
João Vitor Barbosa <joaovitor@jv>
2022-12-03 16:52:18 -0300

    Get categories

---
+++ src/main/java/com/jvbarbosa/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,25 @@
+package com.jvbarbosa.dscommerce.controllers;
+
+import com.jvbarbosa.dscommerce.dto.CategoryDTO;
+import com.jvbarbosa.dscommerce.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> dto = service.findAll();
+        return ResponseEntity.ok(dto);
+    }
+}
---
+++ src/main/java/com/jvbarbosa/dscommerce/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.jvbarbosa.dscommerce.services;
+
+import com.jvbarbosa.dscommerce.dto.CategoryDTO;
+import com.jvbarbosa.dscommerce.entities.Category;
+import com.jvbarbosa.dscommerce.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> result = repository.findAll();
+        return result.stream().map(x -> new CategoryDTO(x)).toList();
+    }
+}
